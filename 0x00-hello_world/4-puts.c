3b5410982a312c50a0b93e5b7825a3d0ff9d4f36
Jose David Caro Cantor <j.caro0207@gmail.com>
2019-09-18 20:25:54 +0000

    Adding 4-task c is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - print information with put
+ *
+ *Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char string [] = "Programming is like building a multilingual puzzle\n";
+	puts (string);
+	return (0);
+
+}
