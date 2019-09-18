e4a01d65b1a9c3c83cdb1e5d6ae7841542db2bdf
Jose David Caro Cantor <j.caro0207@gmail.com>
2019-09-18 20:30:32 +0000

    Fixing 4-task c is fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
  */
 int main (void)
 {
-	char string [] = "Programming is like building a multilingual puzzle\n";
+	char string []= "\"Programming is like building a multilingual puzzle\n";
 	puts (string);
 	return (0);
 
