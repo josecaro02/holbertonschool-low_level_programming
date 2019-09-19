6f602e1130944dd47aabec294afb92e816fb2e87
Jose David Caro Cantor <j.caro0207@gmail.com>
2019-09-19 20:17:57 +0000

    Adding taks 8 project if else while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - print base 16 numbers with putchar
+ *
+ *Return : Alwayss 0 (Success)
+ */
+int main(void)
+{
+	int decimal = 48;
+	int hexa = 97;
+
+	while (decimal < 58)
+	{
+		putchar(decimal);
+		decimal++;
+	}
+	while (hexa < 103)
+	{
+		putchar(hexa);
+		hexa++;
+	}
+	putchar('\n');
+	return (0);
+}
