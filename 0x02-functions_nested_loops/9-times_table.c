a21b29865b3d8a20b4f788cf6a15d10ca48d3f98
Jose David Caro Cantor <j.caro0207@gmail.com>
2019-09-24 06:06:30 +0000

    Fixing task 9 0x02 C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,8 +2,6 @@
 
 /**
  *times_table - prints the 9 times table
- *
- *Return: no return is a void
  */
 void times_table(void)
 {
@@ -36,6 +34,5 @@ void times_table(void)
 		time = 0;
 		nine++;
 	}
-	_putchar('\n');
 
 }
