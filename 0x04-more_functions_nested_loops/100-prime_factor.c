d972354e350abafed6aed5661bb3d615a8376278
josecaro02 <j.caro0207@gmail.com>
2019-09-26 21:19:11 +0000

    Adding 100 task 0x04

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *main - print largest prime factor of a num
+ *
+ *Return: 0 success
+ */
+int main(void)
+{
+	int i;
+	long number, res;
+
+	i = 2;
+	number = 612852475143;
+	res = number;
+	while(res != 1)
+	{
+		if ((res % i) == 0)
+			res = res / i;
+		else
+			i++;
+	}
+		printf("%d\n",i);
+	return (0);
+}
