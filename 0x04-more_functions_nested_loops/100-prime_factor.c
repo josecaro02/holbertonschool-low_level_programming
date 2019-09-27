d7d700050b1dd4603f996f9a61824347449e7834
josecaro02 <j.caro0207@gmail.com>
2019-09-27 00:03:41 +0000

    Deleting stdio from holberton.h

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
  *main - print largest prime factor of a num
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,8 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#include <stdio.h>
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
