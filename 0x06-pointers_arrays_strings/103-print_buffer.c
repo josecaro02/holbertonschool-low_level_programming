f6d06ac1e19b3f837cea1b7babac43081ecab384
josecaro02 <j.caro0207@gmail.com>
2019-10-04 20:23:39 +0000

    adding 103 task to 0x06

---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_buffer - prints a buffer
+ *@b: string with the buffer
+ *@size: tamaño del buffer
+ *
+ *Return: nothing it's a void function
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j, salto10, res;
+
+	salto10 = 1;
+	res = 10 - (size % 10);
+	for (i = 0; i < size + res; i++)
+	{
+		if (salto10 % 10 == 1)
+			printf("%08x: ", salto10 - 1);
+		if (i < size)
+			printf("%02x", b[i]);
+		else
+			printf("  ");
+		if (salto10 % 2 == 0)
+			printf(" ");
+		if (salto10 % 10 == 0)
+		{
+			for (j = i - 9; j <= i; j++)
+			{
+				if (j < size)
+				{
+					if (b[j] >= 32 && b[j] <= 126)
+					{
+						printf("%c", b[j]);
+					}
+					else
+						printf("%c", '.');
+				}
+			}
+			printf("\n");
+		}
+		salto10++;
+	}
+	if (size <= 0)
+		printf("\n");
+}
