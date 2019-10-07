9f72d9f7e18a264150b53670ff2c2371190d4ca5
josecaro02 <j.caro0207@gmail.com>
2019-10-07 17:32:05 +0000

    Adding holberton.h

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <string.h>
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+
+#endif /* HOLBERTON_H */
