f7c138e1c0deeda7ca05276b915559445e949bee
josecaro02 <j.caro0207@gmail.com>
2019-10-07 21:14:51 +0000

    Adding 8 task 0x07

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_diagusms - prints the sum of the two diagonals of a square matrix
+ *@a: matrix given by user
+ *@size: size of matrix
+ *
+ *Return: nothing it's a void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, diag1, diag2;
+
+	diag1 = 0;
+	diag2 = 0;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				diag1 = diag1 + a[(i * size) + j];
+			}
+			if ((i + j) == (size - 1))
+			{
+				diag2 = diag2 + a[(i * size) + j];
+			}
+		}
+	}
+	printf("%d, %d\n",diag1,diag2);
+}
