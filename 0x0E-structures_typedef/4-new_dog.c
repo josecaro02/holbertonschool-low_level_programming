9f5eaa7898884c82722a126bd31f185d27534c25
Jose David Caro <j.caro0207@gmail.com>
2019-10-22 00:37:31 +0000

    Adding free functions to any malloc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,11 +19,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	new_dog->name = malloc(sizeof(char *));
 	if (new_dog->name == NULL)
+	{
+		free(new_dog);
 		return (NULL);
+	}
 	new_dog->name = _strcpy(new_dog->name, name);
 	new_dog->owner = malloc(sizeof(char *));
 	if (new_dog->owner == NULL)
+	{
+		free(new_dog->owner);
+		free(new_dog);
 		return (NULL);
+	}
 	new_dog->owner = _strcpy(new_dog->owner, owner);
 	new_dog->age = age;
 	return (new_dog);
