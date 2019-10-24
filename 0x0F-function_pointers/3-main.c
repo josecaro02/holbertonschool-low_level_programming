fe6768340e6606b0abc76599026e58e5e7b114b8
josecaro02 <j.caro0207@gmail.com>
2019-10-24 04:20:59 +0000

    Fixing segmentation fault in 3-main.c when only 3 arguments are given

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
 	int num1, num2, res;
 	int (*op)(int, int);
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 	if (argc < 4 || argc > 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	if (((*argv[2] == '/' || *argv[2] == '%') && num2 == 0) ||
 argv[2][1] != '\0')
 	{
