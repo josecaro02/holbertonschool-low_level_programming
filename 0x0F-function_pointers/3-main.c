752882529340b86d27ebd66deadbb2a31bdeb811
josecaro02 <j.caro0207@gmail.com>
2019-10-24 04:33:44 +0000

    Returning error 99 if more than 1 parameter is passed in operator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,10 +20,14 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if (((*argv[2] == '/' || *argv[2] == '%') && num2 == 0) ||
-argv[2][1] != '\0')
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
