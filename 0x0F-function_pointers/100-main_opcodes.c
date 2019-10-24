4ad12a903bea7b5e498106e25d9800d8b0f43990
Jose David Caro <j.caro0207@gmail.com>
2019-10-24 17:05:18 +0000

    Complete task 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,7 +9,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int bytes;
+	int bytes, i;
+	unsigned char *p;
+	int (*mn)(int, char **) = main;
 
 	if (argc != 2)
 	{
@@ -17,10 +19,17 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	bytes = atoi(argv[1]);
+	p = (unsigned char *)mn;
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+	for (i = 0; i < bytes - 1; i++)
+	{
+		printf("%02x ", *p);
+		p++;
+	}
+		printf("%02x\n", *p);
 	return (0);
 }
