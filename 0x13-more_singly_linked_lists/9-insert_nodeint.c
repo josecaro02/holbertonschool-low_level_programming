c732ce941adcee67e8b5f0f1208964b137c452fe
Jose David Caro <j.caro0207@gmail.com>
2019-11-05 19:57:46 +0000

    Adding case 0 9-insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,22 +14,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int index;
 
 	index = 0;
-	actual = *head;
-	while (index < idx - 1)
-	{
-		actual  = actual->next;
-		if (actual == NULL)
-			return (NULL);
-		index++;
-	}
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
+	actual = *head;
 	if (idx == 0)
 	{
 		new->next = actual;
 		*head = new;
+	}
+	while (index < idx - 1)
+	{
+		actual  = actual->next;
+		if (actual == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+		index++;
 	}
 	new->next = actual->next;
 	actual->next = new;
