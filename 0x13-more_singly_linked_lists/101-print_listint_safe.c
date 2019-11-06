965868fc1304a5b4ce06b9adfeeda966c21c156d
Jose David Caro <j.caro0207@gmail.com>
2019-11-06 01:36:43 +0000

    Upload lists.h and add 101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ *print_listint_safe - printf a list of elements with address
+ *@head: head of the linked list
+ *
+ *Return: size of the linked list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t n;
+	int *add_a, *add_b;
+
+	n = 0;
+	if (head == NULL)
+		return (n);
+	while (n < 20)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		add_a = (int *)&head;
+		add_b = (int *)&head->next;
+		if (add_a[0] - add_b[0] < 0)
+		{
+			head = head->next;
+			n++;
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+		if(head->next == NULL)
+		{
+			break;
+		}
+		head = head->next;
+		n++;
+	}
+	return (n);
+}
