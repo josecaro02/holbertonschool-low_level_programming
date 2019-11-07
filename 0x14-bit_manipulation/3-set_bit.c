b16449682ef948cd889f2c65a0cccdf808f8cbf2
Jose David Caro <j.caro0207@gmail.com>
2019-11-07 20:39:47 +0000

    fixing task 3 bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,7 +9,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(unsigned long int *) - 1)
+	if (index > sizeof(n) * 8 - 1)
 		return (-1);
 	*n = *n | (1 << index);
 	return (1);
