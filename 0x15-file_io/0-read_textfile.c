1c8bb133ea4c031457a755d23dc7547cefa59745
Jose David Caro <j.caro0207@gmail.com>
2019-11-12 21:23:39 +0000

    Adding README.md, header, 0-main.c and firts task 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ *read_textfile - read a file and prints the letters given
+ *@filename: name of the file to read
+ *@letters: quantity of letters to print
+ *
+ *Return: size of the letters to print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t read_x;
+
+	if (!filename)
+		return (0);
+	buf = malloc(letters + 1);
+	if (buf == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	read_x = read(fd, buf, letters);
+	if (read_x < 0)
+		return (0);
+	write(1, buf, read_x);
+	free(buf);
+	return (read_x);
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOL_H
+#define HOL_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif /* HOL_H */
