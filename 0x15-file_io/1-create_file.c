fa8641d1bab29c52569057ddf5ec2b860340e3a0
Jose David Caro <j.caro0207@gmail.com>
2019-11-12 23:26:32 +0000

    Fixing 1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,6 +12,8 @@ int create_file(const char *filename, char *text_content)
 	int fd, c;
 	ssize_t write_num;
 
+	if (filename == NULL)
+		return (-1);
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
