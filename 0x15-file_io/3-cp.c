fbdb499f36cdb27ee8e04cc72c669078332bb1aa
Jose David Caro <j.caro0207@gmail.com>
2019-11-13 02:08:50 +0000

    Adding 3-cp task

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,55 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ *main - copies the content of a file into another file
+ *@argv: entry arguments
+ *@argc: quantity of arguments
+ *
+ *Return: nothing, it's a void
+ */
+int main(int argc, char *argv[])
+{
+	int fd, fw;
+	char *buf;
+	ssize_t read_num, write_num, close_num, close_new;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	buf = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+		exit(0);
+	fw = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fw == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	while ((read_num = read(fd, buf, 1024)) > 0)
+	{
+		write_num = write(fw, buf, read_num);
+		if (write_num == -1)
+		{
+			dprintf(1, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	close_new = close(fw);
+	close_num = close(fd);
+	if (close_num == -1 || close_new == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd -1\n");
+		exit(100);
+	}
+	return (0);
+}
