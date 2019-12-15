a65e5c674c2a460b35e6d0cb89f4b96347ea7ee3
josecaro02 <j.caro0207@gmail.com>
2019-12-15 23:28:44 +0000

    [Adding option when head is null]

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  *delete_dnodeint_at_index - delete a node at the given index
  *@head: head of the double linked list
@@ -13,7 +12,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int pos;
 
 	pos = 0;
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (-1);
 	actual = *head;
 	if (index == 0 && actual->next != NULL)
