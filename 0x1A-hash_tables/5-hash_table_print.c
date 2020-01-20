0a1cd8da0bff1ff3b7b7de0b894e4971af4ad505
Jose David Caro <j.caro0207@gmail.com>
2020-01-20 01:19:58 +0000

    [Fix task 5] Print {} when hash table is empty Change name of node

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,14 +10,14 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	int hasElement, hasMore;
-	hash_node_t *aux_node;
+	hash_node_t *node;
 
-	hasElement = hasMore= 0;
+	hasElement = hasMore = 0;
 	if (ht != NULL)
 	{
 		for (i = 0; i < ht->size; i++)
 		{
-			if(ht->array[i] == NULL)
+			if (ht->array[i] == NULL)
 				continue;
 			else
 			{
@@ -26,25 +26,26 @@ void hash_table_print(const hash_table_t *ht)
 					printf("{");
 					hasElement = 1;
 				}
-				aux_node = ht->array[i];
-				while (aux_node != NULL)
+				node = ht->array[i];
+				while (node != NULL)
 				{
 					if (hasMore == 0)
 					{
-						printf("\'%s\': \'%s\'"
-						       , aux_node->key,
-						       aux_node->value);
+						printf("\'%s\': \'%s\'",
+						       node->key, node->value);
 						hasMore = 1;
 					}
 					else
-						printf(", \'%s\': \'%s\'"
-						       , aux_node->key,
-						       aux_node->value);
-					aux_node = aux_node->next;
+						printf(", \'%s\': \'%s\'",
+						       node->key, node->value);
+					node = node->next;
 				}
 			}
 		}
 		if (hasElement == 1)
 			printf("}\n");
+		if (hasElement == 0)
+			printf("{}\n");
 	}
+
 }
