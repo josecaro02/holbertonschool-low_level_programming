4907971f6a90733f93baf055bf1b915ee3548dc4
Jose David Caro <j.caro0207@gmail.com>
2020-02-21 20:30:15 -0500

    Update task 5 considering borders as part of island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,13 +14,22 @@ def island_perimeter(grid):
                 if i + 1 < len(grid):
                     if grid[i + 1][j] == 0:
                         perimeter += 1
+                else:
+                    perimeter +=1
                 if i - 1 >= 0:
                     if grid[i - 1][j] == 0:
                         perimeter += 1
+                else:
+                    perimeter +=1
                 if j + 1 < len(grid[0]):
                     if grid[i][j + 1] == 0:
                         perimeter += 1
+                else:
+                    perimeter +=1
                 if j - 1 >= 0:
                     if grid[i][j - 1] == 0:
                         perimeter += 1
+                else:
+                    perimeter +=1
+
     return (perimeter)
