6fc514a4d4bad1a128c7ac29c3604552de3eb191
Jose David Caro <j.caro0207@gmail.com>
2020-02-21 20:31:13 -0500

    [Fix pep8 in task 5]

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,21 +15,21 @@ def island_perimeter(grid):
                     if grid[i + 1][j] == 0:
                         perimeter += 1
                 else:
-                    perimeter +=1
+                    perimeter += 1
                 if i - 1 >= 0:
                     if grid[i - 1][j] == 0:
                         perimeter += 1
                 else:
-                    perimeter +=1
+                    perimeter += 1
                 if j + 1 < len(grid[0]):
                     if grid[i][j + 1] == 0:
                         perimeter += 1
                 else:
-                    perimeter +=1
+                    perimeter += 1
                 if j - 1 >= 0:
                     if grid[i][j - 1] == 0:
                         perimeter += 1
                 else:
-                    perimeter +=1
+                    perimeter += 1
 
     return (perimeter)
