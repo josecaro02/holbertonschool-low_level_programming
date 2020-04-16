701c61511aa71c535f8461c3280ae98795601c7b
Jose David Caro <j.caro0207@gmail.com>
2020-04-16 16:04:04 -0500

    [Update task 100]

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -34,7 +34,7 @@ int jump_search(int *array, size_t size, int value)
 	{
 		a = b;
 		b = b + sqrt(size);
-		printf("1Value checked array[%lu] = [%d]\n", a, array[a]);
+		printf("Value checked array[%lu] = [%d]\n", a, array[a]);
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", a, b);
 	while (a <= min_v(b, size - 1))
