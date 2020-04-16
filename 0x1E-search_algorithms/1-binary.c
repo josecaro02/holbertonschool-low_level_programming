1f13bf4416004ae015b2519c899a681b39322140
Jose David Caro <j.caro0207@gmail.com>
2020-04-16 01:39:02 -0500

    [Update task 1]

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 
 	left = index = 0;
 	right = size - 1;
-	if (array == NULL || value < 0)
+	if (array == NULL || value < array[0])
 		return (-1);
 	while (left <= right)
 	{
