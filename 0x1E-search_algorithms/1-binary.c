337249201a62ab9d2e566f5fd20dc77c62358647
Jose David Caro <j.caro0207@gmail.com>
2020-04-16 01:05:15 -0500

    [Add task 1]

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+/**
+ *binary_search - find a value in an array with binary search
+ *@array: array of integers
+ *@size: size of array
+ *@value: value to find in the array
+ *
+ *Return: index of value, -1 otherwise
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left, right, index, i;
+
+	left = 0;
+	right = size - 1;
+	if (array == NULL)
+		return (-1);
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i != right)
+				printf("%lu, ", i);
+			else
+				printf("%lu\n", i);
+		}
+		index = (left + right) / 2;
+		if (array[index] < value)
+			left = index + 1;
+		else if (array[index] > value)
+			right = index - 1;
+		else
+			return (index);
+	}
+	return (-1);
+}
